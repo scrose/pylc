9cb03969c0743511960218ff6dc0391074a3ff0c
Spencer Rose <spencerrose@uvic.ca>
2020-05-08 09:41:27 -0700

    Slurm sbatch files.

---
+++ jobs/job_hist_merged_3.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH --account=def-ycoady
+#SBATCH --gres=gpu:4
+#SBATCH --cpus-per-task=6
+#SBATCH --mem=32G
+#SBATCH --time=08:00:00
+module load cuda eigen python/3.6
+virtualenv --no-download /home/srose/scratch/vir_env
+source /home/srose/scratch/vir_env/bin/activate
+python ./train.py --model deeplab --capture historic_merged --label hist_merged_3
